d22ff3e686fe4bfa52448b4b80a3197dac1a6386
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2025-12-23 21:50:28 +0700

    add(infra): main code for VPC

--- infra/vpc/variables.tf
+++ infra/vpc/variables.tf
@@ -3,6 +3,11 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 
+variable "ip_internet" {
+    type = string
+    default = "0.0.0.0/0"
+}
+
 variable "public_subnets_cidr" {
     type = list(string)
     default = ["10.0.1.0/24", "10.0.2.0/24"]
