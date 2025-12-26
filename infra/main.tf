f0e59ec4a77032e4a68a7c7038cc5b6d4110a98a
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2025-12-26 19:43:53 +0700

    update(infra): update output iam

--- infra/main.tf
+++ infra/main.tf
@@ -33,14 +33,6 @@ module "security_group" {
   vpc_id = module.vpc.vpc_id
 }
 
-module "alb" {
-  source             = "./modules/alb"
-  vpc_id             = module.vpc.vpc_id
-  alb_sg_id          = module.security_group.alb_sg_id
-  public_subnet_ids  = module.vpc.public_subnet_ids
-  eip_allocation_ids = module.vpc.eip_allocation_ids
-}
-
 module "compute" {
   source = "./modules/compute"
   ec2_instance_profile_name = module.iam.ec2_instance_profile_name
@@ -52,5 +44,5 @@ module "compute" {
 
   private_subnet_ids = module.vpc.private_subnet_ids
   app_sg_id = module.security_group.app_sg_id
-  lb_target_group_arn = module.alb.lb_target_group_arn
+  lb_target_group_arn = module.alb.target_group_arn
 }
