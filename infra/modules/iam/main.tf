0936dd7644edff13ebe3329a58c6ec8f74da8736
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2025-12-26 14:39:54 +0700

    add(infra): add OIDC and role for github actions

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,3 +1,4 @@
+# Setup for EC2 in auto scaling group to pull images from ECR
 resource "aws_iam_role" "ec2_role" {
   name = "ec2-role"
   assume_role_policy = jsonencode({
@@ -19,4 +20,32 @@ resource "aws_iam_role_policy_attachment" "ec2_pull_from_ecr" {
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
+}
+
+# Setup for Github Actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_role" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:sub" = "repo:FajarRamadhanBBX/Bank-Simulation-Deploy-services-with-ALB-and-Scaling-Group-:ref:refs/heads/main"
+        }
+      }
+    }]
+  })
+  
 }
